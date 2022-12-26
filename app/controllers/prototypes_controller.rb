8a6f8f43df6785dac720e8f1bbe0310c7d0f37d9
tamtam-time <even.a.worm.will.tur@gmail.com>
2022-12-26 10:28:48 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,70 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, only:[:edit, :new, :destroy, :show]
+  before_action :move_to_index, except: [:index, :show]
+  before_action :set_prototype, only: [:show, :edit]
+
+  def index
+    @prototypes = Prototype.includes(:user)
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    if
+      Prototype.create(prototype_params)
+        redirect_to root_path
+      else
+        render :new_prototype_path
+    end
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    if
+    prototype.destroy
+      redirect_to root_path
+    end
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    if
+    prototype.update(prototype_params)
+      redirect_to prototype_path(prototype.id)
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    unless user_signed_in? && current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
+  end
+
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
+end
+
